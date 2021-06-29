46e789376c5643a59f6478c330eab743d9c16c91
anthony montreuil <montreuil.anthony@gmail.com>
2021-06-29 17:08:14 +0200

    ajout sql

---
+++ exercicesymfony.sql
@@ -0,0 +1,104 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost
+-- Généré le : mar. 29 juin 2021 à 17:07
+-- Version du serveur : 10.4.19-MariaDB
+-- Version de PHP : 8.0.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `exercicesymfony`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `couleur`
+--
+
+CREATE TABLE `couleur` (
+  `id` int(11) NOT NULL,
+  `title` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `tendance` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `couleur`
+--
+
+INSERT INTO `couleur` (`id`, `title`, `tendance`) VALUES
+(1, 'bleu', 1),
+(2, 'vert', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `course`
+--
+
+CREATE TABLE `course` (
+  `id` int(11) NOT NULL,
+  `title` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `course`
+--
+
+INSERT INTO `course` (`id`, `title`, `status`) VALUES
+(14, 'DEJA VU', 0),
+(15, 'I\'ve just been', 0),
+(16, 'in this place', 0),
+(17, 'before', 0),
+(18, 'Higher on the street', 0),
+(19, 'And i know it\'s', 0),
+(20, 'my time to go', 0);
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `couleur`
+--
+ALTER TABLE `couleur`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `course`
+--
+ALTER TABLE `course`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `couleur`
+--
+ALTER TABLE `couleur`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `course`
+--
+ALTER TABLE `course`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
