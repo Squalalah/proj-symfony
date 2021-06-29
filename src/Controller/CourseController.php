cfaf0c36c5c95654b79551941f303ab0980df41d
anthony montreuil <montreuil.anthony@gmail.com>
2021-06-29 17:04:10 +0200

    Ajouts exercice d'ajd.

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Course;
 use App\Repository\CourseRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class CourseController extends AbstractController
 {
@@ -17,5 +20,32 @@ class CourseController extends AbstractController
             'controller_name' => 'CourseController',
             'courses' => $liste
         ]);
+    }
+    #[Route('/course/delete/{id}', name: 'course_delete')]
+    public function delete(Course $c,EntityManagerInterface $em) : Response {
+        $em->remove($c);
+        $em->flush();
+        return $this->redirectToRoute('course');
+    }
+    #[Route('/course/update/{id}', name: 'course_update')]
+    public function update(Course $c, EntityManagerInterface $em) : Response {
+        $c->setStatus(!$c->getStatus());
+        $em->flush();
+        return $this->redirectToRoute('course');
+
+    }
+
+    #[Route('/course/add', name: 'course_add')]
+    public function addCourse(Request $request, EntityManagerInterface $em) : Response {
+        $title = $request->request->get('_title');
+        if($title != null && strlen($title) > 0) {
+            $title = htmlspecialchars($title);
+            $c = new Course();
+            $c->setStatus(0);
+            $c->setTitle($title);
+            $em->persist($c);
+            $em->flush();
+        }
+        return $this->redirectToRoute('course');
     }
 }
