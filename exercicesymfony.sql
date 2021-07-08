d8f7ec0b5902444bd811f5312030205714e6c7ec
anthony montreuil <montreuil.anthony@gmail.com>
2021-07-08 17:04:51 +0200

    ajout bdd

--- exercicesymfony.sql
+++ exercicesymfony.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : mar. 29 juin 2021 à 17:07
+-- Généré le : jeu. 08 juil. 2021 à 17:04
 -- Version du serveur : 10.4.19-MariaDB
 -- Version de PHP : 8.0.7
 
@@ -21,8 +21,29 @@ SET time_zone = "+00:00";
 -- Base de données : `exercicesymfony`
 --
 
-CREATE DATABASE exercicesymfony;
-USE exercicesymfony;
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categ`
+--
+
+CREATE TABLE `categ` (
+  `id` int(11) NOT NULL,
+  `title` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `categ`
+--
+
+INSERT INTO `categ` (`id`, `title`) VALUES
+(1, 'Anonyme'),
+(2, 'VIP'),
+(3, 'Devs'),
+(4, 'Admin Sys'),
+(5, 'RH'),
+(6, 'Chef de projet'),
+(7, 'Full stack');
 
 -- --------------------------------------------------------
 
@@ -69,9 +90,172 @@ INSERT INTO `course` (`id`, `title`, `status`) VALUES
 (19, 'And i know it\'s', 0),
 (20, 'my time to go', 0);
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `doctrine_migration_versions`
+--
+
+CREATE TABLE `doctrine_migration_versions` (
+  `version` varchar(191) COLLATE utf8_unicode_ci NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `doctrine_migration_versions`
+--
+
+INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
+('DoctrineMigrations\\Version20210701100835', '2021-07-01 10:08:51', 95);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `equipe`
+--
+
+CREATE TABLE `equipe` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `equipe`
+--
+
+INSERT INTO `equipe` (`id`, `title`) VALUES
+(1, 'Equipe Alpha');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `idee`
+--
+
+CREATE TABLE `idee` (
+  `id` int(11) NOT NULL,
+  `idee_category_id` int(11) NOT NULL,
+  `title` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `idee`
+--
+
+INSERT INTO `idee` (`id`, `idee_category_id`, `title`) VALUES
+(1, 1, 'Se barrer aux caraibes'),
+(2, 2, 'Perdre du poids');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `idee_category`
+--
+
+CREATE TABLE `idee_category` (
+  `id` int(11) NOT NULL,
+  `title` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `idee_category`
+--
+
+INSERT INTO `idee_category` (`id`, `title`) VALUES
+(1, 'Travel'),
+(2, 'Sport'),
+(3, 'Entertainment'),
+(4, 'Human Relations'),
+(5, 'Others');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `joueur`
+--
+
+CREATE TABLE `joueur` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `prenom` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `joueur`
+--
+
+INSERT INTO `joueur` (`id`, `nom`, `prenom`) VALUES
+(1, 'Montreuil', 'Anthony'),
+(2, 'Montazac', 'Francois');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `joueur_equipe`
+--
+
+CREATE TABLE `joueur_equipe` (
+  `joueur_id` int(11) NOT NULL,
+  `equipe_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `personne`
+--
+
+CREATE TABLE `personne` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `prenom` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `categ_id_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `personne`
+--
+
+INSERT INTO `personne` (`id`, `nom`, `prenom`, `categ_id_id`) VALUES
+(1, 'Montreuil', 'Anonyme', 1),
+(2, 'Montreuil', 'VIP', 2),
+(3, 'Montreuil', 'Dev', 3),
+(4, 'Montreuil', 'Admin Sys', 4),
+(5, 'Montreuil', 'RH', 5),
+(6, 'Montreuil', 'Chef de projet', 6);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `roles` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`roles`)),
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `roles`, `password`) VALUES
+(1, 'montreuil.anthony@gmail.com', '[\"ROLE_USER\"]', '$2y$13$QUrw7LIYGfm3XwlBifqYTuPZN7rL8bPNVNk0g4.bKw//jr.2fHEKe'),
+(2, 'a@a.a', '[\"ROLE_USER\"]', '$2y$13$Hdli3FIUCxHmkKxClQttWuC/ETRCLVPpnD4lESxAGc72Y17onVWAq'),
+(3, 'b@b.b', '[\"ROLE_USER\"]', '$2y$13$7vN7oY8UbOmNJ7wtWqvqx.q8MJjkDPDvaY4V8t81ftOmJ1qS7JKdS');
+
 --
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `categ`
+--
+ALTER TABLE `categ`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `couleur`
@@ -86,8 +270,67 @@ ALTER TABLE `course`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index pour la table `doctrine_migration_versions`
+--
+ALTER TABLE `doctrine_migration_versions`
+  ADD PRIMARY KEY (`version`);
+
+--
+-- Index pour la table `equipe`
+--
+ALTER TABLE `equipe`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `idee`
+--
+ALTER TABLE `idee`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_DE60E5CEC4F89F6` (`idee_category_id`);
+
+--
+-- Index pour la table `idee_category`
+--
+ALTER TABLE `idee_category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `joueur`
+--
+ALTER TABLE `joueur`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `joueur_equipe`
+--
+ALTER TABLE `joueur_equipe`
+  ADD PRIMARY KEY (`joueur_id`,`equipe_id`),
+  ADD KEY `IDX_CDF2AA99A9E2D76C` (`joueur_id`),
+  ADD KEY `IDX_CDF2AA996D861B89` (`equipe_id`);
+
+--
+-- Index pour la table `personne`
+--
+ALTER TABLE `personne`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_FCEC9EFA37FB39E` (`categ_id_id`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQ_8D93D649E7927C74` (`email`);
+
+--
 -- AUTO_INCREMENT pour les tables déchargées
 --
+
+--
+-- AUTO_INCREMENT pour la table `categ`
+--
+ALTER TABLE `categ`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT pour la table `couleur`
@@ -99,7 +342,66 @@ ALTER TABLE `couleur`
 -- AUTO_INCREMENT pour la table `course`
 --
 ALTER TABLE `course`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+
+--
+-- AUTO_INCREMENT pour la table `equipe`
+--
+ALTER TABLE `equipe`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `idee`
+--
+ALTER TABLE `idee`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `idee_category`
+--
+ALTER TABLE `idee_category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT pour la table `joueur`
+--
+ALTER TABLE `joueur`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `personne`
+--
+ALTER TABLE `personne`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `idee`
+--
+ALTER TABLE `idee`
+  ADD CONSTRAINT `FK_DE60E5CEC4F89F6` FOREIGN KEY (`idee_category_id`) REFERENCES `idee_category` (`id`);
+
+--
+-- Contraintes pour la table `joueur_equipe`
+--
+ALTER TABLE `joueur_equipe`
+  ADD CONSTRAINT `FK_CDF2AA996D861B89` FOREIGN KEY (`equipe_id`) REFERENCES `equipe` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_CDF2AA99A9E2D76C` FOREIGN KEY (`joueur_id`) REFERENCES `joueur` (`id`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `personne`
+--
+ALTER TABLE `personne`
+  ADD CONSTRAINT `FK_FCEC9EFA37FB39E` FOREIGN KEY (`categ_id_id`) REFERENCES `categ` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
